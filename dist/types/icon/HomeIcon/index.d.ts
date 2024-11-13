e7d66abfc8ff6352f2ee72c2ca514d066be0af31
MHG <mhgoodarzi94@gmail.com>
2024-11-13 22:37:03 +0330

    almost stable version

---
+++ dist/types/icon/HomeIcon/index.d.ts
@@ -0,0 +1,3 @@
+import React from "react";
+declare const HomeIcon: React.FC<React.SVGProps<SVGSVGElement>>;
+export default HomeIcon;
---
+++ src/index.d.ts
@@ -0,0 +1,7 @@
+// src/types/index.d.ts
+declare module "nextera-icon-package" {
+  import { FC, SVGProps } from "react";
+
+  export const HomeIcon: FC<SVGProps<SVGSVGElement>>;
+  // Add other icons similarly
+}
